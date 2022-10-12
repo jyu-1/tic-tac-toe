4b25633d58e22f78d780f63790b80fce5d7d3005
jia <jiabids@gmail.com>
2022-10-11 19:48:35 -0700

    add gameReset and draw

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const player = (name) => {
     return { playerName };
 }
 
-const gameBoard = ((name) => {
+const gameBoard = ((p1, p2) => {
 
     const board = ["", "", "", "", "", "", "", "", ""];
 
@@ -12,12 +12,12 @@ const gameBoard = ((name) => {
     const buttons = document.querySelectorAll(".button");
     buttons.forEach((button, index) => {
         button.addEventListener("click", () => {
-            button.innerText = checkTurn(index);
+            button.innerText = gameTurn(index);
             checkWin();
         })
     });
 
-    const checkTurn = (index) => {
+    const gameTurn = (index) => {
         if (board[index] === "O" || board[index] === "X") {
             console.log("Space is Taken");
             return board[index];
@@ -38,32 +38,97 @@ const gameBoard = ((name) => {
 
     const checkWin = () => {
         if (board[0] === board[1] && board[1] === board[2] && board[2] !== "") {
-            console.log("Row 1");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[3] === board[4] && board[4] === board[5] && board[5] !== "") {
-            console.log("Row 2");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[6] === board[7] && board[7] === board[8] && board[8] !== "") {
-            console.log("Row 3");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[0] === board[3] && board[3] === board[6] && board[6] !== "") {
-            console.log("Col 1");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[1] === board[4] && board[4] === board[7] && board[7] !== "") {
-            console.log("Col 2");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[2] === board[5] && board[5] === board[8] && board[8] !== "") {
-            console.log("Col 3");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[0] === board[4] && board[4] === board[8] && board[8] !== "") {
-            console.log("Diag 1");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
         else if (board[2] === board[4] && board[4] === board[6] && board[6] !== "") {
-            console.log("Diag 2");
+            if (turn === 1) {
+                alert("Player 2 Wins");
+                resetGame();
+            }
+            else {
+                alert("Player 1 Wins");
+                resetGame();
+            }
         }
-        else {
-            return;
+        else if (board[0] !== "" && board[1] !== "" && board[2] !== "" && board[3] !== "" && board[4] !== "" && board[5] !== "" && board[6] !== "" && board[7] !== "" && board[8] !== "") {
+            alert("Draw!");
+            resetGame();
         }
+    };
+
+    const resetGame = () => {
+        buttons.forEach((button, index) => {
+            button.innerText = "";
+            board[index] = "";
+        })
+        turn = 1;
     };
 
 
