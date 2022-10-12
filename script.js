9fcc182f9db7a1e4123ef798720ea49e12bb9cea
jia <jiabids@gmail.com>
2022-10-11 19:27:45 -0700

    store gameboard in array instead of html

--- script.js
+++ script.js
@@ -5,63 +5,64 @@ const player = (name) => {
 
 const gameBoard = ((name) => {
 
-    const board = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+    const board = ["", "", "", "", "", "", "", "", ""];
 
     let turn = 1;
 
     const buttons = document.querySelectorAll(".button");
-    buttons.forEach((button) => {
+    buttons.forEach((button, index) => {
         button.addEventListener("click", () => {
-            button.innerText = checkTurn(button.innerText);
-            checkWin(buttons);
+            button.innerText = checkTurn(index);
+            checkWin();
         })
     });
 
-    const checkTurn = (button) => {
-        if (button === "O" || button === "X") {
+    const checkTurn = (index) => {
+        if (board[index] === "O" || board[index] === "X") {
             console.log("Space is Taken");
-            return button;
+            return board[index];
         }
         else {
             if (turn === 1) {
                 turn = 2;
+                board[index] = "O";
                 return "O";
             }
             else if (turn === 2) {
                 turn = 1;
+                board[index] = "X";
                 return "X";
             }
         }
     };
 
-    const checkWin = (button) => {
-        if (button[0].innerText === button[1].innerText && button[1].innerText === button[2].innerText && button[2].innerText !== "0") {
+    const checkWin = () => {
+        if (board[0] === board[1] && board[1] === board[2] && board[2] !== "") {
             console.log("Row 1");
         }
-        else if (button[3].innerText === button[4].innerText && button[4].innerText === button[5].innerText && button[5].innerText !== "0") {
+        else if (board[3] === board[4] && board[4] === board[5] && board[5] !== "") {
             console.log("Row 2");
-            
         }
-        else if (button[6].innerText === button[7].innerText && button[7].innerText === button[8].innerText && button[8].innerText !== "0") {
+        else if (board[6] === board[7] && board[7] === board[8] && board[8] !== "") {
             console.log("Row 3");
         }
-        else if (button[0].innerText === button[3].innerText && button[3].innerText === button[6].innerText && button[6].innerText !== "0") {
+        else if (board[0] === board[3] && board[3] === board[6] && board[6] !== "") {
             console.log("Col 1");
         }
-        else if (button[1].innerText === button[4].innerText && button[4].innerText === button[7].innerText && button[7].innerText !== "0") {
+        else if (board[1] === board[4] && board[4] === board[7] && board[7] !== "") {
             console.log("Col 2");
         }
-        else if (button[2].innerText === button[5].innerText && button[5].innerText === button[8].innerText && button[8].innerText !== "0") {
+        else if (board[2] === board[5] && board[5] === board[8] && board[8] !== "") {
             console.log("Col 3");
         }
-        else if (button[0].innerText === button[4].innerText && button[4].innerText === button[8].innerText && button[8].innerText !== "0") {
+        else if (board[0] === board[4] && board[4] === board[8] && board[8] !== "") {
             console.log("Diag 1");
         }
-        else if (button[2].innerText === button[4].innerText && button[4].innerText === button[6].innerText && button[6].innerText !== "0") {
+        else if (board[2] === board[4] && board[4] === board[6] && board[6] !== "") {
             console.log("Diag 2");
         }
         else {
-
+            return;
         }
     };
 
